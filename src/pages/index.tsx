210c2d64f2bc3419a08b57edd08422fcaa444df3
Valerie Andy <valerie@codedifferently.com>
2022-08-13 14:23:58 -0400

    Updating front end for submission

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,157 +1,72 @@
-import { FC } from 'react'
-import { APP_NAME } from '@/lib/consts'
-import ConnectWallet from '@/components/ConnectWallet'
-import { BookOpenIcon, CodeIcon, ShareIcon } from '@heroicons/react/outline'
-import ThemeSwitcher from '@/components/ThemeSwitcher'
+import CircularProgress from '@/components/CircularProgress'
+import React, { useState } from 'react'
 
-const Home: FC = () => {
+const inputStyles =
+	'w-full outline-none border border-slate-200 dark:border-slate-700 bg-transparent px-5 py-2.5 rounded-lg focus:border-blue-600 duration-150'
+export default function App() {
+	const [isLoading, setIsLoading] = useState(false)
+
+	const [inputField, setInputField] = useState('')
+	const [output, setOutput] = useState('')
+
+	const onSubmitHandler = (e: React.FormEvent) => {
+		e.preventDefault()
+
+		setIsLoading(true)
+
+		setTimeout(() => {
+			setIsLoading(false)
+
+			setOutput('Output will show here')
+		}, 1500)
+	}
 	return (
-		<div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-			<div className="absolute top-6 right-6">
-				<ConnectWallet />
-			</div>
-			<ThemeSwitcher className="absolute bottom-6 right-6" />
-			<div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
-				<div className="flex justify-center pt-8 sm:justify-start sm:pt-0">
-					<h1 className="text-6xl font-bold dark:text-white">{APP_NAME}</h1>
-				</div>
-
-				<div className="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-					<div className="grid grid-cols-1 md:grid-cols-2">
-						<div className="p-6">
-							<div className="flex items-center">
-								<BookOpenIcon className="w-8 h-8 text-gray-500" />
-								<div className="ml-4 text-lg leading-7 font-semibold">
-									<a
-										href="https://laravel.com/docs"
-										className="underline text-gray-900 dark:text-white"
-									>
-										Next.js Docs
-									</a>
-								</div>
-							</div>
-
-							<div className="ml-12">
-								<div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-									Next.js gives you the best developer experience with all the features you need for
-									production: hybrid static &amp; server rendering, TypeScript support, smart
-									bundling, route pre-fetching, and more. No config needed.
-								</div>
-							</div>
-						</div>
-
-						<div className="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-							<div className="flex items-center">
-								<BookOpenIcon className="w-8 h-8 text-gray-500" />
-								<div className="ml-4 text-lg leading-7 font-semibold">
-									<a href="https://wagmi.sh" className="underline text-gray-900 dark:text-white">
-										wagmi Docs
-									</a>
-								</div>
-							</div>
-
-							<div className="ml-12">
-								<div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-									wagmi is a collection of React Hooks containing everything you need to start working
-									with Ethereum. wagmi makes it easy to display ENS and balance information, sign
-									messages, interact with contracts, and much more — all with caching, request
-									deduplication, and persistence.
-								</div>
-							</div>
-						</div>
-
-						<div className="p-6 border-t border-gray-200 dark:border-gray-700">
-							<div className="flex items-center">
-								<BookOpenIcon className="w-8 h-8 text-gray-500" />
-								<div className="ml-4 text-lg leading-7 font-semibold">
-									<a
-										href="https://laravel-news.com/"
-										className="underline text-gray-900 dark:text-white"
-									>
-										Tailwind Docs
-									</a>
-								</div>
-							</div>
-
-							<div className="ml-12">
-								<div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-									Tailwind CSS is a highly customizable, low-level CSS framework that gives you all of
-									the building blocks you need to build bespoke designs without any annoying
-									opinionated styles you have to fight to override.
-								</div>
-							</div>
-						</div>
-
-						<div className="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-							<div className="flex items-center">
-								<CodeIcon className="w-8 h-8 text-gray-500" />
-								<div className="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
-									About this Template
-								</div>
-							</div>
-
-							<div className="ml-12">
-								<div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-									This starter kit is composed of{' '}
-									<a href="https://nextjs.org" className="underline" target="_blank" rel="noreferrer">
-										Next.js
-									</a>{' '}
-									and{' '}
-									<a
-										href="https://tailwindcss.com"
-										className="underline"
-										target="_blank"
-										rel="noreferrer"
-									>
-										Tailwind CSS
-									</a>
-									, with{' '}
-									<a
-										href="https://docs.family.co/connectkit"
-										className="underline"
-										target="_blank"
-										rel="noreferrer"
-									>
-										ConnectKit
-									</a>
-									,{' '}
-									<a href="https://ethers.org" className="underline" target="_blank" rel="noreferrer">
-										ethers
-									</a>{' '}
-									&amp;{' '}
-									<a href="https://wagmi.sh" className="underline" target="_blank" rel="noreferrer">
-										wagmi
-									</a>{' '}
-									for all your web3 needs. It uses{' '}
-									<a
-										href="https://www.typescriptlang.org/"
-										className="underline"
-										target="_blank"
-										rel="noreferrer"
-									>
-										Typescript
-									</a>{' '}
-									and an opinionated directory structure for maximum dev confy-ness. Enjoy!
-								</div>
-							</div>
-						</div>
+		<div className="flex justify-center items-center flex-col min-h-screen">
+			<div className="border border-slate-200 dark:border-slate-700 rounded-xl bg-white dark:bg-slate-900 shadow-md overflow-hidden">
+				<h1 className="text-center text-2xl font-medium px-7 py-4 bg-blue-600 text-white">
+					Enter any 256-bit hexadecimal value
+				</h1>
+				<div className="relative py-8">
+					<form onSubmit={onSubmitHandler} className="flex gap-5 px-7">
+						<input
+							className={inputStyles}
+							placeholder="Input"
+							type="text"
+							id="hashInput"
+							value={inputField}
+							onChange={e => setInputField(e.target.value)}
+						/>
+						<button
+							type="submit"
+							className="text-white shrink-0 bg-blue-600 hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-7 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 "
+						>
+							Calculate
+						</button>
+					</form>
+					<div className="px-7 mt-6">
+						<label htmlFor="hashOutput" className="text-base font-medium inline-block mb-2">
+							Output:
+						</label>
+						<br />
+						<input
+							type="text"
+							id="hashOutput"
+							value={output}
+							onChange={e => setOutput(e.target.value)}
+							className={inputStyles}
+						/>
+						<br />
 					</div>
-				</div>
 
-				<div className="flex justify-center mt-4 sm:items-center sm:justify-between">
-					<div className="text-center text-sm text-gray-500 sm:text-left">
-						<div className="flex items-center">
-							<ShareIcon className="-mt-px w-5 h-5 text-gray-400" />
-
-							<a href="https://twitter.com/m1guelpf" className="ml-1 underline">
-								Share
-							</a>
+					{isLoading && (
+						<div className="absolute top-0 left-0 w-full h-full bg-blue-500 flex items-center justify-center z-10 text-white">
+							<CircularProgress size={35} />
 						</div>
-					</div>
+					)}
 				</div>
 			</div>
 		</div>
 	)
 }
-
-export default Home
+// Log to console
+console.log('Hello console')
